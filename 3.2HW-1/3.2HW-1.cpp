9dd87af749390f0414eabe9c274fc12dff6e6f3f
Louise Omarova <ruizu.om21@gmail.com>
2024-10-11 14:38:54 +0300

    HW is ready

---
+++ 3.2HW-1/3.2HW-1.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <string>
+
+class Calculator
+{
+private:
+	double num1;
+	double num2;
+
+public:
+	double add()
+	{
+		return num1 + num2;
+	}
+	double multiply()
+	{
+		return num1 * num2;
+	}
+	double subtract_1_2()
+	{
+		return num1 - num2;
+	}
+	double subtract_2_1()
+	{
+		return num2 - num1;
+	}
+	double divide_1_2() //на нуль делить нельзя. if
+	{
+		return num1 / num2;
+		if (num2 == 0)
+		{
+			std::cout << "Division by 0 is impossible!" << std::endl;
+			return 0;
+		}
+	}
+	double divide_2_1() //на нуль делить нельзя. if
+	{
+		return num2 / num1;
+		if (num1 == 0)
+		{
+			std::cout << "Division by 0 is impossible!" << std::endl;
+			return 0;
+		}
+	}
+
+	bool set_num1(double num1)
+	{
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		return false;
+	}
+
+	bool set_num2(double num2)
+	{
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		return false;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	Calculator calculator;
+
+	while (true)
+	{
+		double num1;
+		double num2;
+
+		std::cout << "Enter num1: ";
+		std::cin >> num1;
+		if (!calculator.set_num1(num1))
+		{
+			std::cout << "Inavalid input!" << std::endl;
+			continue;
+		}
+
+		std::cout << "Enter num2: ";
+		std::cin >> num2;
+		if (!calculator.set_num2(num2))
+		{
+			std::cout << "Inavalid input!" << std::endl;
+			continue;
+		}
+
+		std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+		std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+		std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+		std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+		std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+	}
+
+	return 0;
+}
---
+++ 3.2HW-2/3.2HW-2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+
+class Counter
+{
+private: 
+	int value;
+
+public:
+	Counter() : value(1) {}
+
+	Counter(int initValue) : value(initValue) {}
+
+	void increment()
+	{
+		++value;
+	}
+
+	void decrement()
+	{
+		--value;
+	}
+
+	int getValue()
+	{
+		return value;
+	}
+};
+
+
+int main(int argc, char** argv)
+{
+	std::string userAnswer;
+
+	std::cout << "Do you want to specify the initial value of the counter ? Enter yes or no : ";
+	std::cin >> userAnswer;
+
+	Counter counter;
+
+	if (userAnswer == "yes")
+	{
+		int userValue;
+		std::cout << "Enter the initial counter value: ";
+		std::cin >> userValue;
+		counter = Counter(userValue);
+	}
+	
+	char command;
+	while (true)
+	{
+		std::cout << "Enter the command('+', '-', '=' or 'x') :";
+		std::cin >> command;
+
+		if (command == '+')
+		{
+			counter.increment();
+		}
+		else if (command == '-')
+		{
+			counter.decrement();
+		}
+		else if (command == '=')
+		{
+			std::cout << counter.getValue()<< std::endl;
+		}
+		else if (command == 'x')
+		{
+			std::cout << "Goodbye!" << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid command.";
+		}
+	}
+
+	return 0;
+}
